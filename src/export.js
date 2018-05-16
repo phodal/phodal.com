caf96c251fb797f85a9b2117fc0aabd3ff3beb50
Phodal HUANG <h@phodal.com>
2018-05-16 21:37:02 +0800

    [T] init base export scripts

---
+++ src/export.js
@@ -0,0 +1,11 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('dev.db', 'sqlite3.OPEN_READONLY');
+
+db.serialize(function () {
+  db.each("SELECT * FROM `blog_blogpost`", function (err, row) {
+    console.log(err);
+    console.log(row);
+  });
+});
+
+db.close();
